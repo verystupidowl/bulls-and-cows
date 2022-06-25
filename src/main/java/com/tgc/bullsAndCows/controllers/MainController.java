3b86096ba56397dd7cfdd98415b984dafde228ee
Alexandra <amaklov2002@gmail.com>
2022-06-25 17:43:00 +0400

    optimize imports

--- src/main/java/com/tgc/bullsAndCows/controllers/MainController.java
+++ src/main/java/com/tgc/bullsAndCows/controllers/MainController.java
@@ -9,7 +9,7 @@ import com.tgc.bullsAndCows.service.PlayerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.*;
+import java.util.List;
 
 @RestController
 @RequestMapping("/game")
